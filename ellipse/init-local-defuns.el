7357cdac8332c6108713df5539c5bbec121d84b5
ellipse <ellipse423@gmail.com>
2013-02-26 23:32:05 +0800

    personal little func

--- ellipse/init-local-defuns.el
+++ ellipse/init-local-defuns.el
@@ -49,22 +49,53 @@
 
 
 ;; unscroll for wrong scrolling
-(defvar unscroll-point nil
+(defvar unscroll-point (make-marker)
   "Cursor position for next call to 'unscroll'.")
-(defvar unscroll-window-start nil
+(defvar unscroll-window-start (make-marker)
   "Window start for next call to 'unscroll'.")
+(defvar unscroll-hscroll nil
+  "Hscroll for next call to 'unscroll'.")
+(progn
+  (set-marker unscroll-point nil)
+  (set-marker unscroll-window-start nil)
+  (put 'scroll-up 'unscrollable t)
+  (put 'scroll-down 'unscrollable t)
+  (put 'scroll-left 'unscrollable t)
+  (put 'scroll-right 'unscrollable t))
+(defun unscroll-maybe-remember ()
+  (if (not (get last-command 'unscrollable))
+      (progn
+        (set-marker unscroll-point (point))
+        (set-marker unscroll-window-start (window-start))
+        (setq unscroll-hscroll (window-hscroll)))))
+
 (defadvice scroll-up (before remember-for-unscroll
                              activate compile)
   "Remember where we started from, for 'unscroll'."
-  (if (not (eq last-command 'scroll-up))
-      (progn
-        (setq unscroll-point (point))
-        (setq unscroll-window-start (window-start)))))
+  (unscroll-maybe-remember))
+(defadvice scroll-down (before remember-for-unscroll
+                               activate compile)
+  "Remember where we started from, for 'unscroll'."
+  (unscroll-maybe-remember))
+(defadvice scroll-left (before remember-for-unscroll
+                               activate compile)
+  "Remember where we started from, for 'unscroll'."
+  (unscroll-maybe-remember))
+(defadvice scroll-right (before remember-for-unscroll
+                                activate compile)
+  "Remember where we started from, for 'unscroll'."
+  (unscroll-maybe-remember))
+
 (defun unscroll ()
   "Jump to location specified by 'unscroll-to'."
   (interactive)
-  (goto-char unscroll-to)
-  (set-window-start nil unscroll-window-start))
+  (if (not unscroll-point)
+      (error "Can't unscroll yet"))
+  (goto-char unscroll-point)
+  (set-window-start nil unscroll-window-start)
+  (set-window-hscroll nil unscroll-hscroll))
 (global-set-key "\C-x\M-v" 'unscroll)
 
 (provide 'init-local-defuns)
+;; WRITTEN: Sun Feb 03 09:28:58 2013 .
+
