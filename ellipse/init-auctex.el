62634f5da264daca3317a1c44ac9ed8e1931eb2c
ellipse <ellipse423@gmail.com>
2013-03-12 14:27:20 +0800

    remove some unusable hooks

--- ellipse/init-auctex.el
+++ ellipse/init-auctex.el
@@ -3,18 +3,21 @@
 
 (mapc (lambda (mode)
       (add-hook 'LaTeX-mode-hook mode))
-      (list 'auto-fill-mode
+      (list ;; 'auto-fill-mode
             'LaTeX-math-mode
             'turn-on-reftex
-            'linum-mode))
+            'linum-mode
+            'auto-complete-mode
+            'autopair-mode))
 
 (add-hook 'LaTeX-mode-hook
           (lambda ()
-            (setq TeX-auto-untabify t     ; remove all tabs before saving
+            (setq ;;TeX-auto-untabify t     ; remove all tabs before saving
                   TeX-engine 'xetex       ; use xelatex default
                   TeX-show-compilation t) ; display compilation windows
             (TeX-global-PDF-mode t)       ; PDF mode enable, not plain
             (setq TeX-save-query nil)
             (imenu-add-menubar-index)
-            (define-key LaTeX-mode-map (kbd "TAB") 'TeX-complete-symbol)))
+            ;;(define-key LaTeX-mode-map (kbd "TAB") 'TeX-complete-symbol)
+            ))
 (provide 'init-auctex)
