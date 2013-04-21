0776300002971a9a51ca1070a775002f04c28bec
ellipse <ellipse423@gmail.com>
2013-04-21 22:57:25 +0800

    init these two modes in separate files

---
+++ ellipse/init-mit-scheme.el
@@ -0,0 +1,5 @@
+(load-library "xscheme")
+;(add-hook 'scheme-mode-hook 'autopair-mode)
+(add-hook 'scheme-mode-hook 'auto-complete-mode)
+
+(provide 'init-mit-scheme)
---
+++ ellipse/init-sml-mode.el
@@ -0,0 +1,5 @@
+(require 'sml-mode-autoloads)
+;(add-hook 'sml-mode-hook 'autopair-mode)
+(add-hook 'sml-mode-hook 'auto-complete-mode)
+
+(provide 'init-sml-mode)
